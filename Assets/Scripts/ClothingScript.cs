ef67ba177f8fae0151beffc354bb0a1e41a14417
Chris <93794624+Foxify52@users.noreply.github.com>
2025-05-06 20:07:48 -0400

    Part 11

---
+++ Assets/Scripts/ClothingScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Viva
+{
+
+
+    public abstract class ClothingScript : MonoBehaviour
+    {
+
+        public abstract void OnBeginWearing(Companion companion);
+        public abstract void OnApplywearing();
+    }
+
+}
