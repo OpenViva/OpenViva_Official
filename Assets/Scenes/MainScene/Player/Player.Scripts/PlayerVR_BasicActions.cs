79e91558c4887e673a51883fc23404b694cd8c40
saidu39 <saiennaidu99@gmail.com>
2025-08-15 23:04:26 +0200

    Revert "V0.1.2.5"
    This reverts commit 5fe9d11727bbff9add2c999f04f78d41cc45bfa4.

--- Assets/Scenes/MainScene/Player/Player.Scripts/PlayerVR_BasicActions.cs
+++ Assets/Scenes/MainScene/Player/Player.Scripts/PlayerVR_BasicActions.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
-public class PlayerVR_BasicActions : MonoBehaviour
+public class PlayerVR_BasicActions: MonoBehaviour
 {
     // This class detects a VR player's inputs and executes the correct response.
-    // Code by Saien
 
     // FIELDS
     [SerializeField] private InputActionReference[] inputActionReferences = new InputActionReference[3];
