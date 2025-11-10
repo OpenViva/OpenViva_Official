b81c6148c25d734d638bbf37d4e5e761ddc03257
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2025-11-11 01:22:37 +0200

    Fixed camera jitter

--- Assets/Scenes/MainScene/Player/Player.Scripts/PlayerKB_Movement.cs
+++ Assets/Scenes/MainScene/Player/Player.Scripts/PlayerKB_Movement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerKB_Movement : MonoBehaviour
 {
@@ -8,34 +9,78 @@ public class PlayerKB_Movement : MonoBehaviour
     // --- Movement ---
     [Header("Movement")]
     [SerializeField, Range(1f, 20f)] private float _movementSpeed;
-
     [Tooltip("How much faster do you want to go when running?")]
     [SerializeField, Range(1f, 20f)] private float _runMultiplier;
+
+    // --- Look ---
+    [Header("Look")]
+    [Range(1f, 20f)]
+    [Tooltip("speed of the camera movement")]
+    [SerializeField] private float _mouseSensitivity = 10;
 
     // --- Jump & Gravity ---
     [Header("Jump & Gravity")]
     [SerializeField, Range(1f, 20f)] private float _jumpHeight;
-
     [SerializeField] private float _gravity = -9.81f;
 
     // --- References ---
     [Header("References")]
     [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Transform _camera;
 
-    // --- Fields ---
+    // --- Debug ---
+    [Header("Debug")]
     public Vector2 moveInput;
     public Vector2 lookInput;
+
+    // --- Fields ---
+    private float _xRotation;
+    private float _yRotation;
     private Vector3 _controllerVelocity;
     private bool _isRunning = false;
+
+    private void Start()
+    {
+        // Locks cursor and makes it invisible
+        Cursor.lockState = Cursor.lockState;
+        Cursor.visible = false;
+
+        if (TryGetComponent(out CharacterController foundController))
+        {
+            _characterController = foundController;
+        }
+        else Debug.LogWarning($"Character Controller of {this} cannot be assigned!");
+    }
 
     void Update()
     {
         HandleGravity();
+        HandleLook();
 
         Vector3 move = (transform.right * moveInput.x + transform.forward * moveInput.y) * GetCurrentSpeed();
         Vector3 totalMove = (move + _controllerVelocity) * Time.deltaTime;
 
         _characterController.Move(totalMove);
+    }
+
+    private void LateUpdate()
+    {
+        // rotates the controller on the y-axis so that it is on the same rotation as the camera
+        transform.localRotation = Quaternion.Euler(0f, _yRotation, 0f);
+
+        // rotates camera on the y- and x-axis
+        _camera.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+    }
+
+    void HandleLook()
+    {
+        if (lookInput == Vector2.zero) return;
+
+        _yRotation += lookInput.x * _mouseSensitivity * Time.deltaTime;
+        _xRotation -= lookInput.y * _mouseSensitivity * Time.deltaTime;
+
+        // Rotate camera up/down with clamp
+        _xRotation = Mathf.Clamp(_xRotation, -90, 90);
     }
 
     public void HandleJump()
