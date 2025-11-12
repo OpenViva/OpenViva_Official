7f6fad95607983d09c47743ba80849404b4ec863
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2025-11-12 22:03:10 +0200

    Added Globals for global states

---
+++ Assets/Scenes/MainScene/Scripts/Persistance/Globals.cs
@@ -0,0 +1,12 @@
+public static class Globals
+{
+    // --- UI ---
+    public static bool isMenuOpen = false;
+
+    // --- Movement ---
+    public static bool handleMovement = true;
+    public static bool handleKBLook = true;
+
+    // --- Toggles ---
+    public static bool isDesktopMode = true;
+}
