779ceb41fdfdc06892f6e7bdb12ea88df18b1ec6
saidu39 <saiennaidu99@gmail.com>
2025-12-08 16:34:13 +0200

    Implemented Skyboxes
    Skyboxes will transition between each other as the day progresses in the game. The speed at which this transition occurs depends on the set Cycle Speed.
    
    Note: The skyboxes themselves need to be changed. They look pretty weird atm. The evening skybox is yellow

--- Assets/Scenes/MainScene/Scripts/DayNightCycle.cs
+++ Assets/Scenes/MainScene/Scripts/DayNightCycle.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
@@ -21,40 +23,77 @@ public class DayNightCycle : MonoBehaviour
     private int days;
     private int _days { get { return days; } set { days = value; } }
 
+    [SerializeField] Texture2D morningSkybox;
+    [SerializeField] Texture2D daySkybox;
+    [SerializeField] Texture2D afternoonSkybox;
+    [SerializeField] Texture2D eveningSkybox;
+    [SerializeField] Texture2D nightSkybox;
+    private Boolean isTransitioning = false;
+    private float transitionSpeed = 0f;
+
     private void Start()
     {
         _minutes = 60 * hours;
         changeSunIntensity(_minutes);
-        revolutionPoint.transform.Rotate(hours * 15, 0, 0);
+        revolutionPoint.transform.Rotate(hours * 15f, 0, 0);
 
         switch (speed)
         {
             case CycleSpeeds.Speed.FiveMinutes:
                 increment = 360f / (5 * 60);
+                transitionSpeed = 25f;
                 break;
 
             case CycleSpeeds.Speed.TwentyMinutes:
                 increment = 360f / (20 * 60);
+                transitionSpeed = 100f;
                 break;
 
             case CycleSpeeds.Speed.OneHour:
                 increment = 360f / (1 * 60 * 60);
+                transitionSpeed = 300f;
                 break;
 
             case CycleSpeeds.Speed.ThreeHours:
                 increment = 360f / (3 * 60 * 60);
+                transitionSpeed = 900f;
                 break;
 
             case CycleSpeeds.Speed.SixHours:
                 increment = 360f / (6 * 60 * 60);
+                transitionSpeed = 1800f;
                 break;
 
             case CycleSpeeds.Speed.TwelveHours:
                 increment = 360f / (12 * 60 * 60);
+                transitionSpeed = 3600f;
                 break;
 
             case CycleSpeeds.Speed.OneDay:
                 increment = 360f / (24 * 60 * 60);
+                transitionSpeed = 7200f;
+                break;
+        }
+
+        switch (_minutes)
+        {
+            case int n when (n >= 0 && n < 300):
+                RenderSettings.skybox.SetTexture("_Texture1", nightSkybox);
+                break;
+            case int n when (n >= 300 && n < 420):
+                RenderSettings.skybox.SetTexture("_Texture1", morningSkybox);
+                break;
+            case int n when (n >= 420 && n < 720):
+                RenderSettings.skybox.SetTexture("_Texture1", daySkybox);
+                break;
+            case int n when (n >= 720 && n < 1020):
+                RenderSettings.skybox.SetTexture("_Texture1", afternoonSkybox);
+                break;
+            case int n when (n >= 1020 && n < 1140):
+                RenderSettings.skybox.SetTexture("_Texture1", eveningSkybox);
+                break;
+            case int n when (n >= 1140 && n < 1440):
+                RenderSettings.skybox.SetTexture("_Texture1", nightSkybox);
                 break;
         }
     }
@@ -81,6 +120,27 @@ public class DayNightCycle : MonoBehaviour
         {
             minutes = 0;
         }
+
+        if (value == 300)
+        {
+            StartCoroutine(PhaseTransition(nightSkybox, morningSkybox));
+        }
+        else if (value == 420)
+        {
+            StartCoroutine(PhaseTransition(morningSkybox, daySkybox));
+        }
+        else if (value == 720)
+        {
+            StartCoroutine(PhaseTransition(daySkybox, afternoonSkybox));
+        }
+        else if (value == 1020)
+        {
+            StartCoroutine(PhaseTransition(afternoonSkybox, eveningSkybox));
+        }
+        else if (value == 1140)
+        {
+            StartCoroutine(PhaseTransition(eveningSkybox, nightSkybox));
+        }
     }
 
     private void OnHourChange(int value)
@@ -91,6 +151,25 @@ public class DayNightCycle : MonoBehaviour
             _days++;
             revolutionPoint.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
+
+    }
+
+    private IEnumerator PhaseTransition(Texture2D from, Texture2D to)
+    {
+        isTransitioning = true;
+
+        RenderSettings.skybox.SetTexture("_Texture1", from);
+        RenderSettings.skybox.SetTexture("_Texture2", to);
+        RenderSettings.skybox.SetFloat("_Blend", 0);
+
+        for (float i = 0; i < transitionSpeed; i += Time.deltaTime)
+        {
+            RenderSettings.skybox.SetFloat("_Blend", i / transitionSpeed);
+            yield return null;
+        }
+
+        RenderSettings.skybox.SetTexture("_Texture1", to);
+        isTransitioning = false;
     }
 
     private void changeSunIntensity(int value)
