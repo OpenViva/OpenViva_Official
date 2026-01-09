d2cc54b23d570696b4b6a4997d18eb8af4766d60
saidu39 <saiennaidu99@gmail.com>
2026-01-09 16:50:06 +0200

    Inventory System
    The bag can now be used to store up to 10 objects tagged as "Item".
    
    Instructions:
    - With the bag in either hand, press [Q] to open/close the bag.
    - With your free hand, pick up item.
    - With the bag open, press [LMB] or [RMB] (depending on which hand the bag is in) to place the item in the bag. For example, if the bag is in your left hand, press [LMB].
    - Use the scrollwheel to select an item from the bag.
    - Press [E] to take the selected item.
    - You can drop the bag if it is closed
    
    Note: You cannot extend or retract your hands while the bag is open.
    Note 2: There are a few things I still need to iron out, but nothing the player will notice.

---
+++ Assets/Scenes/MainScene/Scripts/Item/Inventory.cs
@@ -0,0 +1,179 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Inventory : MonoBehaviour
+{
+    private PlayerKB_GrabObject _bagScript; // Script to check which hand the bag is held in
+    private int _grabbedIn = 0; // 0 - Neither, 1 - Left, 2 - Right
+    [SerializeField] private PlayerManager _playerManager; // The player manager script for checking what item the player is holding
+    private int _itemInOtherHand = -1;
+    private Animator _animator; // Animator used to open or close the bag
+    private DesktopInput _keybinds;
+    private bool _isOpen; // Check if the bag is open
+    [SerializeField] private List<string> _inventory = new List<string>();
+    private int _numElements = 0;
+    [SerializeField] private List<GameObject> _objects = new List<GameObject>();
+    private ItemIndexes _itemIndexes;
+    private int _selectedItem = 1;
+    [SerializeField] PlayerKB_BasicActions _basicActions;
+
+    private void Start()
+    {
+        _bagScript = GetComponent<PlayerKB_GrabObject>();
+        _animator = GetComponent<Animator>();
+
+        _keybinds = new DesktopInput();
+        _keybinds.Viva.Enable();
+        _keybinds.Viva.ToggleBag.performed += ToggleBag;
+        _keybinds.Viva.LeftGrab.performed += PlaceInBagWithRightHand;
+        _keybinds.Viva.RightGrab.performed += PlaceInBagWithLeftHand;
+        _keybinds.Viva.ScrollUp.performed += ScrollUp;
+        _keybinds.Viva.ScrollDown.performed += ScrollDown;
+        _keybinds.Viva.TakeFromBag.performed += TakeSelectedItem;
+
+        _itemIndexes = new ItemIndexes();
+    }
+
+    private void Update()
+    {
+        _grabbedIn = _bagScript.GetIsGrabbed();
+        _bagScript.SetIsOpen(_isOpen);
+        if (_grabbedIn != 0)
+        {
+            if (_grabbedIn == 1)
+            {
+                _itemInOtherHand = _playerManager.GetItemRight();
+            }
+            else if ( _grabbedIn == 2)
+            {
+                _itemInOtherHand = _playerManager.GetItemLeft();
+            }
+        }
+    }
+
+    private void ToggleBag(InputAction.CallbackContext context)
+    {
+        if (_grabbedIn > 0)
+        {
+            if (_isOpen)
+            {
+                _animator.Play("Close");
+            }
+            else
+            {
+                _animator.Play("Open");
+            }
+            _isOpen = ! _isOpen;
+            _basicActions.SetBagOpen(_isOpen);
+        }
+    }
+
+    private void PlaceInBagWithRightHand(InputAction.CallbackContext context)
+    {
+        if (_isOpen && _grabbedIn == 1 && _numElements < 10)
+        {
+            string itemName = _itemIndexes.GetItemName(_itemInOtherHand);
+            if (itemName != "Error")
+            {
+                _inventory.Add(itemName);
+                GameObject item = _playerManager.GetObjectRight();
+                if (item != null)
+                {
+                    _objects.Add(item);
+                    PlayerKB_GrabObject itemScript = item.GetComponent<PlayerKB_GrabObject>();
+                    itemScript.SetIsActive(false, 0);
+                    _numElements++;
+                }
+                
+            }  
+        }
+    }
+
+    private void PlaceInBagWithLeftHand(InputAction.CallbackContext context)
+    {
+        if (_isOpen && _grabbedIn == 2 && _numElements < 10)
+        {
+            string itemName = _itemIndexes.GetItemName(_itemInOtherHand);
+            if (itemName != "Error")
+            {
+                _inventory.Add(itemName);
+                GameObject item = _playerManager.GetObjectLeft();
+                if (item != null)
+                {
+                    _objects.Add(item);
+                    PlayerKB_GrabObject itemScript = item.GetComponent<PlayerKB_GrabObject>();
+                    itemScript.SetIsActive(false, 0);
+                    _numElements++;
+                }
+            }
+        }
+    }
+
+    private void ScrollUp(InputAction.CallbackContext context)
+    {
+        if (!_isOpen || _numElements == 0) return;
+
+        if (_selectedItem < _numElements)
+        {
+            _selectedItem++;
+            Debug.Log("Selected Item: " + _inventory[_selectedItem - 1]);
+        }
+    }
+
+    private void ScrollDown(InputAction.CallbackContext context)
+    {
+        if (!(_isOpen && _numElements > 0)) return;
+
+        if (_selectedItem > 1)
+        {
+            _selectedItem--;
+            Debug.Log("Selected Item: " + _inventory[_selectedItem - 1]);
+        }
+    }   
+
+    private void TakeSelectedItem(InputAction.CallbackContext context)
+    {
+        if (_isOpen && _numElements > 0)
+        {
+            GameObject item = _objects[_selectedItem - 1];
+            _objects.Remove(item);
+            _inventory.RemoveAt(_selectedItem - 1);
+            PlayerKB_GrabObject itemScript = item.GetComponent<PlayerKB_GrabObject>();
+            if (_grabbedIn == 1)
+            {
+                itemScript.SetIsActive(true, 2);
+            }
+            else if (_grabbedIn == 2)
+            {
+                itemScript.SetIsActive(true, 1);
+            }
+            _numElements--;
+
+            if (_numElements == 0)
+            {
+                _selectedItem = 1;
+            }
+            else if (_selectedItem >= _numElements)
+            {
+                _selectedItem = _numElements - 1;
+            }
+
+            if (_numElements != 0)
+            {
+                Debug.Log("Selected Item: " + _inventory[_selectedItem]);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _keybinds.Viva.ToggleBag.performed -= ToggleBag;
+        _keybinds.Viva.LeftGrab.performed -= PlaceInBagWithRightHand;
+        _keybinds.Viva.RightGrab.performed -= PlaceInBagWithLeftHand;
+        _keybinds.Viva.ScrollUp.performed -= ScrollUp;
+        _keybinds.Viva.ScrollDown.performed -= ScrollDown;
+        _keybinds.Viva.TakeFromBag.performed -= TakeSelectedItem;
+        _keybinds.Viva.Disable();
+    }
+}
--- Assets/Scenes/MainScene/Scripts/PlatformDependant/Desktop/PlayerKB_GrabObject.cs
+++ Assets/Scenes/MainScene/Scripts/PlatformDependant/Desktop/PlayerKB_GrabObject.cs
@@ -7,115 +7,180 @@ using UnityEngine.InputSystem;
 
 public class PlayerKB_GrabObject : MonoBehaviour
 {
-    [SerializeField] private InputActionReference[] inputActionReferences = new InputActionReference[2]; // 0 - LBM, 1 - RMB
-    private GameObject playerLeftHand; // The player's left hand object
-    private GameObject playerRightHand; // The player's right hand object
-    private Collider playerLeftCollider; // The collider of the player's left hand
-    private Collider playerRightCollider; // The collider of the player's right hand
-    private GameObject grabbableObject; // The object to be grabbed
-    private Rigidbody grabbableObjectRB; // The Rigidbody of the object to be grabbed
+    [SerializeField] private InputActionReference[] _inputActionReferences = new InputActionReference[2]; // 0 - LBM, 1 - RMB
+    private GameObject _playerLeftHand; // The player's left hand object
+    private GameObject _playerRightHand; // The player's right hand object
+    private Collider _playerLeftCollider; // The collider of the player's left hand
+    private Collider _playerRightCollider; // The collider of the player's right hand
+    private GameObject _grabbableObject; // The object to be grabbed
+    private Rigidbody _grabbableObjectRB; // The Rigidbody of the object to be grabbed
 
-    private bool playerInRange = false; // Check whether the player is in range to grab the object
-    private bool isGrabbedInLeft = false; // Check whether the object is grabbed in the left hand
-    private bool isGrabbedInRight = false; // Check whether the object is grabbed in the right hand
+    private bool _playerInRange = false; // Check whether the player is in range to grab the object
+    private bool _isGrabbedInLeft = false; // Check whether the object is grabbed in the left hand
+    private bool _isGrabbedInRight = false; // Check whether the object is grabbed in the right hand
 
-    private ObjectHoldPositions holdPositions = new ObjectHoldPositions(); // Calls the script that holds the positions and rotations of all grabbable objects
-    [SerializeField] private int objectIndex; // The index of the objec in the ObjectHoldPositions script
+    private ObjectHoldPositions _holdPositions = new ObjectHoldPositions(); // Calls the script that holds the positions and rotations of all grabbable objects
+    [SerializeField] private int _objectIndex; // The index of the object in the ObjectHoldPositions script
+    private bool _isActive = true; // The item cannot be grabbed if this is false
+    private bool _isOpen = false; // If this object is not a bag, this variable is always true
 
     private void Start()
     {
         // Find the player's hand objects in the scene
-        playerLeftHand = GameObject.Find("hand_l");
-        playerRightHand = GameObject.Find("hand_r");
+        _playerLeftHand = GameObject.Find("hand_l");
+        _playerRightHand = GameObject.Find("hand_r");
 
         // Set the grabbable object and its Rigidbody
-        grabbableObject = this.gameObject;
-        grabbableObjectRB = grabbableObject.GetComponent<Rigidbody>();
+        _grabbableObject = this.gameObject;
+        _grabbableObjectRB = _grabbableObject.GetComponent<Rigidbody>();
 
         // Set the colliders of the player's hands
-        playerLeftCollider = playerLeftHand.GetComponent<Collider>();
-        playerRightCollider = playerRightHand.GetComponent<Collider>();
+        _playerLeftCollider = _playerLeftHand.GetComponent<Collider>();
+        _playerRightCollider = _playerRightHand.GetComponent<Collider>();
 
         // Enable input actions and bind the grab functions
-        for (int i = 0; i < inputActionReferences.Length; i++)
+        for (int i = 0; i < _inputActionReferences.Length; i++)
         {
-            inputActionReferences[i].action.Enable();
+            _inputActionReferences[i].action.Enable();
         }
-        inputActionReferences[0].action.performed += grabLeft;
-        inputActionReferences[1].action.performed += grabRight;
+        _inputActionReferences[0].action.performed += grabLeft;
+        _inputActionReferences[1].action.performed += grabRight;
     }
 
     private void grabLeft(InputAction.CallbackContext context)
     {
         // If the player is in range and the object is not already grabbed, grab it with the left hand
-        if (playerInRange && !isGrabbedInLeft && !isGrabbedInRight)
+        if (_isActive && !_isOpen)
         {
-            grabbableObjectRB.useGravity = false;
-            grabbableObjectRB.isKinematic = true;
-            grabbableObject.transform.SetParent(playerLeftHand.transform);
-            grabbableObject.transform.localPosition = holdPositions.GetObjectPositionLeft(objectIndex);
-            grabbableObject.transform.localRotation = holdPositions.GetObjectRotationLeft(objectIndex);
-            isGrabbedInLeft = true;
-        }
-        // If the object is already grabbed in the left hand, release it
-        else if (isGrabbedInLeft)
-        {
-            grabbableObject.transform.SetParent(null);
-            grabbableObjectRB.isKinematic = false;
-            grabbableObjectRB.useGravity = true;
-            isGrabbedInLeft = false;
+            if (_playerInRange && !_isGrabbedInLeft && !_isGrabbedInRight)
+            {
+                _grabbableObjectRB.useGravity = false;
+                _grabbableObjectRB.isKinematic = true;
+                _grabbableObject.transform.SetParent(_playerLeftHand.transform);
+                _grabbableObject.transform.localPosition = _holdPositions.GetObjectPositionLeft(_objectIndex);
+                _grabbableObject.transform.localRotation = _holdPositions.GetObjectRotationLeft(_objectIndex);
+                _isGrabbedInLeft = true;
+            }
+            // If the object is already grabbed in the left hand, release it
+            else if (_isGrabbedInLeft)
+            {
+                _grabbableObject.transform.SetParent(null);
+                _grabbableObjectRB.isKinematic = false;
+                _grabbableObjectRB.useGravity = true;
+                _isGrabbedInLeft = false;
+            }
         }
     }
 
     private void grabRight(InputAction.CallbackContext context)
     {
         // If the player is in range and the object is not already grabbed, grab it with the right hand
-        if (playerInRange && !isGrabbedInRight && !isGrabbedInLeft)
+        if (_isActive && !_isOpen)
         {
-            grabbableObjectRB.useGravity = false;
-            grabbableObjectRB.isKinematic = true;
-            grabbableObject.transform.SetParent(playerRightHand.transform);
-            grabbableObject.transform.localPosition = holdPositions.GetObjectPositionRight(objectIndex);
-            grabbableObject.transform.localRotation = holdPositions.GetObjectRotationRight(objectIndex);
-            isGrabbedInRight = true;
-        }
-        // If the object is already grabbed in the right hand, release it
-        else if (isGrabbedInRight)
-        {
-            grabbableObject.transform.SetParent(null);
-            grabbableObjectRB.isKinematic = false;
-            grabbableObjectRB.useGravity = true;
-            isGrabbedInRight = false;
+            if (_playerInRange && !_isGrabbedInRight && !_isGrabbedInLeft)
+            {
+                _grabbableObjectRB.useGravity = false;
+                _grabbableObjectRB.isKinematic = true;
+                _grabbableObject.transform.SetParent(_playerRightHand.transform);
+                _grabbableObject.transform.localPosition = _holdPositions.GetObjectPositionRight(_objectIndex);
+                _grabbableObject.transform.localRotation = _holdPositions.GetObjectRotationRight(_objectIndex);
+                _isGrabbedInRight = true;
+            }
+            // If the object is already grabbed in the right hand, release it
+            else if (_isGrabbedInRight)
+            {
+                _grabbableObject.transform.SetParent(null);
+                _grabbableObjectRB.isKinematic = false;
+                _grabbableObjectRB.useGravity = true;
+                _isGrabbedInRight = false;
+            }
         }
     }
 
     private void OnTriggerEnter(Collider collider)
     {
         // Check if the player is in range to grab the bag
-        if (collider == playerLeftCollider || collider == playerRightCollider)
+        if (collider == _playerLeftCollider || collider == _playerRightCollider)
         {
-            playerInRange = true;
+            _playerInRange = true;
         }
     }
 
     private void OnTriggerExit(Collider collider)
     {
         // Check if the player is out of range to grab the bag
-        if (collider == playerLeftCollider || collider == playerRightCollider)
+        if (collider == _playerLeftCollider || collider == _playerRightCollider)
         {
-            playerInRange = false;
+           _playerInRange = false;
         }
+    }
+
+    // Checks if the bag is being held and in which hand
+    public int GetIsGrabbed()
+    {
+        if (_isGrabbedInLeft)
+        {
+            return 1;
+        } 
+        else if (_isGrabbedInRight)
+        {
+            return 2;
+        }
+        return 0;
+    }
+
+    public void SetIsActive(bool set, int handedness)
+    {
+        if (set == false)
+        {
+            _grabbableObject.transform.SetParent(null);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+            if (handedness == 1)
+            {
+                _grabbableObject.transform.SetParent(_playerLeftHand.transform);
+                _grabbableObject.transform.localPosition = _holdPositions.GetObjectPositionLeft(_objectIndex);
+                _grabbableObject.transform.localRotation = _holdPositions.GetObjectRotationLeft(_objectIndex);
+                _isGrabbedInLeft = true;
+                _isGrabbedInRight = false;
+            }
+            else
+            {
+                _grabbableObject.transform.SetParent(_playerRightHand.transform);
+                _grabbableObject.transform.localPosition = _holdPositions.GetObjectPositionRight(_objectIndex);
+                _grabbableObject.transform.localRotation = _holdPositions.GetObjectRotationRight(_objectIndex);
+                _isGrabbedInRight = true;
+                _isGrabbedInLeft = false;
+            }
+        }
+
+        _grabbableObjectRB.isKinematic = set;
+        _grabbableObjectRB.useGravity = !set;
+        _isActive = set;
+        if (set == false)
+        {
+            _isGrabbedInLeft = set;
+            _isGrabbedInRight = set;
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void SetIsOpen(bool isOpen)
+    {
+        _isOpen = isOpen;
     }
 
     void OnDestroy()
     {
         // Disable input actions and unbind the grab functions
-        for (int i = 0; i < inputActionReferences.Length; i++)
+        for (int i = 0; i < _inputActionReferences.Length; i++)
         {
-            inputActionReferences[i].action.Disable();
+            _inputActionReferences[i].action.Disable();
         }
-        inputActionReferences[0].action.performed -= grabLeft;
-        inputActionReferences[1].action.performed -= grabRight;
+        _inputActionReferences[0].action.performed -= grabLeft;
+        _inputActionReferences[1].action.performed -= grabRight;
     }
 }
 #endif
