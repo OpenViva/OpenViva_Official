6b77e23eec1ae4ca167804323540dc98eb248f0d
saidu39 <saiennaidu99@gmail.com>
2026-01-10 16:21:03 +0200

    New Item: Flour Jar
    No usability yet.

--- Assets/Scenes/MainScene/Scripts/PlatformDependant/Desktop/ObjectHoldPositions.cs
+++ Assets/Scenes/MainScene/Scripts/PlatformDependant/Desktop/ObjectHoldPositions.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ObjectHoldPositions : MonoBehaviour
 {
-    private Vector3[] allObjectPositionsLeft = new Vector3[9]
+    private Vector3[] allObjectPositionsLeft = new Vector3[10]
     {
         new Vector3(0.0456f, 0.0211f, -0.0027f), // BAG
         new Vector3(0.0133f, 0.0137f, -0.007f), // RUBBER_DUCKY
@@ -12,10 +12,11 @@ public class ObjectHoldPositions : MonoBehaviour
         new Vector3(-0.00514069f, 0.03108077f, -0.0057312f), // BLUEBERRY
         new Vector3(0.01392059f, 0.01097836f, 0.01483068f), // WHEAT
         new Vector3(0.0041f, 0.0192f, -0.0057f), // FLASHLIGHT
-        new Vector3(0.0003204332f, 0.0162733f, -0.006679615f) // EGG
+        new Vector3(0.0003204332f, 0.0162733f, -0.006679615f), // EGG
+        new Vector3(0.01903992f, 0.02049595f, -0.02319194f) // FLOUR_JAR
     };
 
-    private Vector3[] allObjectPositionsRight = new Vector3[9]
+    private Vector3[] allObjectPositionsRight = new Vector3[10]
     {
         new Vector3(-0.04559939f, 0.02110242f, -0.003600158f), // BAG
         new Vector3(-0.004600528f, 0.00939743f, -0.007199669f), // RUBBER_DUCKY
@@ -25,10 +26,11 @@ public class ObjectHoldPositions : MonoBehaviour
         new Vector3(0.0051f, 0.0305f, -0.006f), // BLUEBERRY
         new Vector3(-0.01840058f, 0.008688283f, 0.01550032f), // WHEAT
         new Vector3(-0.004100041f, 0.01919305f, -0.005302102f), // FLASHLIGHT
-        new Vector3(-0.0003005998f, 0.01638018f, -0.008376584f) // EGG
+        new Vector3(-0.0003005998f, 0.01638018f, -0.008376584f), // EGG
+        new Vector3(-0.01903993f, 0.0204898f, -0.02080939f) // FLOUR_JAR
     };
 
-    private Quaternion[] allObjectRotationsLeft = new Quaternion[9]
+    private Quaternion[] allObjectRotationsLeft = new Quaternion[10]
     {
         Quaternion.Euler(0f, 90f, 0f), // BAG
         Quaternion.Euler(-15.865f, 22.223f, 34.353f), // RUBBER_DUCKY
@@ -38,10 +40,11 @@ public class ObjectHoldPositions : MonoBehaviour
         Quaternion.Euler(4.003f, 24.241f, 91.8f), // BLUEBERRY
         Quaternion.Euler(16.234f, 33.554f, 100.504f), // WHEAT
         Quaternion.Euler(-180f, 90f, 180f), // FLASHLIGHT
-        Quaternion.Euler(-107.063f, 91.058f, -1.106018f) // EGG
+        Quaternion.Euler(-107.063f, 91.058f, -1.106018f), // EGG
+        Quaternion.Euler(-15.914f, -180f, -90f) // FLOUR_JAR
     };
 
-    private Quaternion[] allObjectRotationsRight = new Quaternion[9]
+    private Quaternion[] allObjectRotationsRight = new Quaternion[10]
     {
         Quaternion.Euler(0f, -90f, 0f), // BAG
         Quaternion.Euler(0f, 159.498f, 9.144f), // RUBBER_DUCKY
@@ -51,7 +54,8 @@ public class ObjectHoldPositions : MonoBehaviour
         Quaternion.Euler(183.722f, 147.97f, -87.67401f), // BLUEBERRY
         Quaternion.Euler(9.406f, -43.707f, 101.126f), // WHEAT
         Quaternion.Euler(-180f, 270f, 180f), // FLASHLIGHT
-        Quaternion.Euler(-107.063f, -88.94202f, -1.106018f) // EGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+        Quaternion.Euler(-107.063f, -88.94202f, -1.106018f), // EGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+        Quaternion.Euler(-15.914f, 0f, -90f) // FLOUR_JAR
     };
 
     public Vector3 GetObjectPositionLeft(int objectIndex)
