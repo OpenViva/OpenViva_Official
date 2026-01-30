a0f3d6b9d119012d39c59fcbf3c87c7cc0bd107b
saidu39 <saiennaidu99@gmail.com>
2026-01-30 17:46:03 +0200

    Item Hold Animations - Part 3
    - Knife
    - Lantern
    - Milk Canister
    - Mixing Bowl
    - Mixing Spoon

--- Assets/Scenes/MainScene/Scripts/PlatformDependant/Desktop/ObjectHoldPositions.cs
+++ Assets/Scenes/MainScene/Scripts/PlatformDependant/Desktop/ObjectHoldPositions.cs
@@ -5,7 +5,7 @@ public class ObjectHoldPositions : MonoBehaviour
 {
     private Vector3[] allObjectPositionsLeft = new Vector3[20]
     {
-        new Vector3(0.0907f, -0.2307f, 0.1391f), // BAG
+        new Vector3(0.0479f, 0.0124f, -0.005f), // BAG
         new Vector3(0.0133f, 0.0137f, -0.007f), // RUBBER_DUCKY
         new Vector3(0.0025f,0.0158f, -0.0064f), // PEACH
         new Vector3(0.00077f, 0.03047f, -0.0053f), // STRAWBERRY
@@ -19,7 +19,7 @@ public class ObjectHoldPositions : MonoBehaviour
         new Vector3(-0.00038f, 0.01314f, -0.00221f), // LANTERN
         new Vector3(0.0253f, 0.0135f, -0.0092f), // MILK_CANISTER
         new Vector3(0.008180236f, 0.02287848f, -0.0223377f), // MIXING_BOWL
-        new Vector3(-0.0057f, 0.0359f, -0.0119f), // MIXING_SPOON
+        new Vector3(-0.00536f, 0.01277f, -0.00911f), // MIXING_SPOON
         new Vector3(0.01464081f, 0.02148611f, -0.01569432f), // MORTAR
         new Vector3(-0.0052f, 0.01671f, -0.01126f), // PESTLE
         new Vector3(0.0065f, 0.0135f, -0.0221f), // POT
@@ -38,12 +38,12 @@ public class ObjectHoldPositions : MonoBehaviour
         new Vector3(-0.01840058f, 0.008688283f, 0.01550032f), // WHEAT
         new Vector3(-0.004100041f, 0.01919305f, -0.005302102f), // FLASHLIGHT
         new Vector3(-0.0003005998f, 0.01638018f, -0.008376584f), // EGG
-        new Vector3(-0.01903993f, 0.0204898f, -0.02080939f), // FLOUR_JAR
+        new Vector3(-0.019f, 0.0218f, -0.0035f), // FLOUR_JAR
         new Vector3(0f, 0.0255f, -0.004f), // KNIFE
         new Vector3(-0.00221f, 0.0097f, -0.0018f), // LANTERN
         new Vector3(-0.0253f, 0.0154f, -0.0103f), // MILK_CANISTER
         new Vector3(-0.008180243f, 0.02286628f, -0.02166189f), // MIXING_BOWL
-        new Vector3(0.003947446f, 0.03384247f, -0.01147585f), // MIXING_SPOON
+        new Vector3(0.004679678f, 0.01651493f, -0.0108921f), // MIXING_SPOON
         new Vector3(-0.01255f, 0.02265f, -0.01679f), // MORTAR
         new Vector3(0.0052f, 0.0167f, -0.0111f), // PESTLE
         new Vector3(-0.006500248f, 0.0134955f, -0.02190186f), // POT
@@ -53,7 +53,7 @@ public class ObjectHoldPositions : MonoBehaviour
 
     private Quaternion[] allObjectRotationsLeft = new Quaternion[20]
     {
-        Quaternion.Euler(90f, 90f, -90f), // BAG
+        Quaternion.Euler(0f, 90f, 0f), // BAG
         Quaternion.Euler(-15.865f, 22.223f, 34.353f), // RUBBER_DUCKY
         Quaternion.Euler(-5.64f, 4.661f, 90.595f), // PEACH
         Quaternion.Euler(0f, 0f, 90f), // STRAWBERRY
@@ -64,10 +64,10 @@ public class ObjectHoldPositions : MonoBehaviour
         Quaternion.Euler(-107.063f, 91.058f, -1.106018f), // EGG
         Quaternion.Euler(0f, -180f, -90f), // FLOUR_JAR
         Quaternion.Euler(-90f, 0, 90f), // KNIFE
-        Quaternion.Euler(0f, -90f, 90f), // LANTERN
+        Quaternion.Euler(0f, -90f, 0f), // LANTERN
         Quaternion.Euler(90f, 0f, 90f), // MILK_CANISTER
         Quaternion.Euler(0f, 0f, 90f), // MIXING_BOWL
-        Quaternion.Euler(-12.754f, -5.818f, 18.995f), // MIXING_SPOON
+        Quaternion.Euler(-4.232f, -13.827f, 73.916f), // MIXING_SPOON
         Quaternion.Euler(107.547f, -61.47198f, 28.91901f), // MORTAR
         Quaternion.Euler(0f, 0f, 90f), // PESTLE
         Quaternion.Euler(0f, 0f, 90f), // POT
@@ -91,7 +91,7 @@ public class ObjectHoldPositions : MonoBehaviour
         Quaternion.Euler(0f, 90f, 90f), // LANTERN
         Quaternion.Euler(90f, -180f, 90f), // MILK_CANISTER
         Quaternion.Euler(0f, 180f, 90f), // MIXING_BOWL
-        Quaternion.Euler(-18.041f, -372.479f, -14.093f), // MIXING_SPOON
+        Quaternion.Euler(-12.754f, -166.1f, 73.91f), // MIXING_SPOON
         Quaternion.Euler(107.547f, 118.528f, 28.91901f), // MORTAR
         Quaternion.Euler(0f, 180f, 90f), // PESTLE
         Quaternion.Euler(0f, 180f, 90f), // POT
