a1f1bd347622001f802bb9a4069ed28e14b3c6a8
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2026-02-12 17:06:12 +0200

    Code cleanup

--- Assets/Scenes/MainScene/Scripts/PlatformDependant/XR/PlayerVR_GrabObject.cs
+++ Assets/Scenes/MainScene/Scripts/PlatformDependant/XR/PlayerVR_GrabObject.cs
@@ -1,8 +1,6 @@
-using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class PlayerVR_GrabObject : XRGrabInteractable
 {
