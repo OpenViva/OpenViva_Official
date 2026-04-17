e0c00a7246eb95283e99b8ffc43fd2c1e360d9d3
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2026-04-17 22:53:10 +0300

    Added code TODO

--- Assets/Scripts/Characters/Testing/NavAgentController.cs
+++ Assets/Scripts/Characters/Testing/NavAgentController.cs
@@ -4,7 +4,7 @@ using UnityEngine.AI;
 
 public class NavAgentController : MonoBehaviour
 {
-    public Vector3 coords; // Change this to move to mouse look position
+    public Vector3 coords; // TODO: Change this to move to mouse look position
 
     private NavMeshAgent agent;
     private Animator animator;
