badcd95a102e6660c96f373488e1d31433959528
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2026-06-02 21:20:38 +0300

    Prepare character builder

---
+++ Assets/Scripts/Characters/Testing/CharacterAssembler.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAssembler : MonoBehaviour
+{
+    #region Fields
+    private List<GameObject> loadedCharacters = new List<GameObject>();
+    private bool isLoading = false;
+
+    #endregion
+
+
+}
