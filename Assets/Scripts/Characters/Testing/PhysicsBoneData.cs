834e6f22ed4f584afca0c04278e4921cedfc1299
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2026-06-02 23:06:15 +0300

    Added bone data class

---
+++ Assets/Scripts/Characters/Testing/PhysicsBoneData.cs
@@ -0,0 +1,16 @@
+public class PhysicsBoneData
+{
+    // TODO: Edit this to fit the game needs
+    public string boneName;
+    public string preset;
+    public float gravity;
+    public float damping;
+    public float distanceCompression;
+    public float stiffnessValue;
+    public bool useStiffnessCurve;
+    public float stiffnessCurveStart;
+    public float stiffnessCurveEnd;
+    public float velocityAttenuation;
+    public bool useLimit;
+    public float speedLimit;
+}
